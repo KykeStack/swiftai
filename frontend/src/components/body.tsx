3cce1b7667025af6826b7dd1efce7eb6fd76a99c
Julio Orellana <coppro.cman@proton.me>
2023-11-25 03:52:40 +0100

    Update Component Styles

--- frontend/src/components/body.tsx
+++ frontend/src/components/body.tsx
@@ -2,19 +2,21 @@ import { Component } from 'solid-js'
 import SearchConfigBar from './search-config-bar'
 import VideoRealTime from './video-real-time'
 import CarouselScreenshots from './carousel-screenshots'
+import VideoPreviewBox from './video-preview-box'
+import DataSection from './data-section'
 
 const Body: Component<{}> = (props) => {
   return (
-    <div class='flex flex-row gap-4 w-screen h-screen overflow-scroll overflow-x-hidden pb-10'>
-      <div class='h-fit w-full'>
+    <div class='flex flex-row gap-4 w-screen h-screen overflow-scroll overflow-x-hidden pb-10 pr-10'>
+      <div class='h-fit w-[55rem]'>
         <h1 class='text-5xl dark:text-white text-black not-italic font-bold mt-20 ml-10'>Dashboard</h1>
         <SearchConfigBar />
         <VideoRealTime />
         <CarouselScreenshots />
       </div>
-      <div class='w-[40rem]'>
-        <VideoRealTime />
-        <SearchConfigBar />
+      <div class='h-fit w-[40rem]'>
+        <VideoPreviewBox />
+        <DataSection />
       </div>
     </div>
   )
