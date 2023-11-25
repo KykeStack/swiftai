14be21961803635b38c253c7023de1e04d36adbc
Julio Orellana <coppro.cman@proton.me>
2023-11-25 11:48:08 +0100

    Refactor Main Page

--- frontend/src/components/main-page.tsx
+++ frontend/src/components/main-page.tsx
@@ -1,10 +1,12 @@
 import { Component } from 'solid-js'
 import SideBar from './side-bar'
 import Body from './body'
+import GeneralModal from './general-modal'
 
 const MainPage: Component<{}> = (props) => {
   return (
     <div class='w-screen h-screen flex flex-row'>
+      <GeneralModal />
       <SideBar />
       <Body />
     </div>
